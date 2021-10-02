4a85ed73da3b30f4cd07304fd383334dd4ca154b
mikhail.deakonov <mikhail.deakonov@unidata-platform.org>
2021-10-02 14:41:56 +0300

    revision2

--- lab3/src/main/java/ru/mondayish/Runner.java
+++ lab3/src/main/java/ru/mondayish/Runner.java
@@ -4,6 +4,7 @@ import ru.mondayish.managers.ConsoleManager;
 
 public class Runner {
     public static void main(String[] args) {
+        System.out.println("revision 2");
         new ConsoleManager().run();
     }
 }
